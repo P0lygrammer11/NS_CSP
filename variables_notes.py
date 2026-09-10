7fb89ff5118d67961b55248db80a780b13df7c31
P0lygrammer <nargis.shahidani@ucas-edu.net>
2026-09-10 14:34:04 -0600

    variables practices

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+#NS Variable notes
+name = input("what your name?")
+
+print(name)
+print("CS is the best class ever!!!!!!!!!!!")
+print("i wana leave rn!!")
+print ("hellow" , name, "welcome to class")
---
+++ variables_practive.py
@@ -0,0 +1,12 @@
+#NS, variables practice
+import random 
+user = input("whats your name").strip()
+num1 = random.randint(1,100)
+num2 =random.randint(100,1000)
+breakfast = input(" what did you have for breakfast?").strip()
+fave_color = input("whats you favorite color?").strip()
+school = input("whast your school name?")
+year = input("what year is it?")
+eyes = input("whats your eye color?")
+age = input("how old is your friend?")
+subject = input("whats your fvorite subject?")
