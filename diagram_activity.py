e4b15cd1f4b6c22b4dfc79f17ce59ab6a551a25c
P0lygrammer <nargis.shahidani@ucas-edu.net>
2026-09-14 14:52:21 -0600

    idkkkkk

---
+++ diagram_activity.py
@@ -0,0 +1,17 @@
+# NS, diagram activity
+name = "Alex"
+print(f"\nthe varaible Alex has a value of {name} and is saved at {id(name)}\n")
+age = 14
+print(f"\nthe varaible 14 has a value of {age} and is saved at{id(age)}\n")
+score1 = 25
+print(f"\nthe varaible 10 has a value of {score1} and is saved at{id(score1)}\n")
+height = 5.9
+print(f"\nthe varaible 5.9 has a value of {height} and is saved at{id(height)}\n")
+city = "Denvor"
+print(f"\nthe varaible Denvor has a value of {city} and is saved at{id(city)}\n")
+zip_code = 80202
+print(f"\nthe varaible 80202 has a value of {zip_code} and is saved at{id(zip_code)}\n")
+price1 = 10
+print(f"\nthe varaible 10 has a value of {price1} and is saved at{id(price1)}\n")
+price2 = 10
+print(f"\nthe varaible 10 has a value of {price1} and is saved at{id(price1)}\n")
---
+++ logical_memory.py
@@ -0,0 +1,5 @@
+#NS< Logical memory
+pi = 3.149159
+school = "UCAS"
+print(f"\nThe variable pi has the value of (pi) and is saved at {id(pi)}\n")
+print(f"\nthe varaible school has a value of {school} and is saved at{id(school)}\n")
