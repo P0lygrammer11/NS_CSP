f0539bcb3d9a05f13420777a17997ae288ecdfff
P0lygrammer <nargis.shahidani@ucas-edu.net>
2026-09-16 14:52:20 -0600

    jgkjdsg

---
+++ expressions.py
@@ -0,0 +1,34 @@
+# NS. 7th integer, Floats, and expressing notes
+
+ #integers
+people = 24
+cars = 50
+computers = 29
+awareness = -12
+
+# float
+pi = 3.14
+temp = -95.6
+cost = 1.99
+rain = 2.17
+
+# Arithmetic Operators (+ - * / // ** %)
+
+print(f"18/4 is {18/4} or {18//4} with a remainer of {18%4}")
+print(f"18/5 is {18/5} or {18//5} with a remainer of {18%5}")
+
+# order of operations
+grades = [85,66,94,72]
+students = len(grades)
+averege = sum(grades)/students
+
+print(f"The averege is {int(averege)}")
+
+#convert data type
+price = float(input("How much did the item cost? "))
+tax = 0.0485
+sales_tax = price*tax
+total = price + sales_tax
+print(f" your total is {total}")
+
+
