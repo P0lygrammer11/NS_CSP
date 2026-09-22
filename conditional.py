bf8996bb80636b88a24523321f1d501f2c036402
P0lygrammer <nargis.shahidani@ucas-edu.net>
2026-09-22 14:52:59 -0600

    oihoidhoihyodhohd oih doioids hoih doid soi hods

---
+++ conditional.py
@@ -0,0 +1,20 @@
+#NS, Conditional Notes
+
+# conditional 1
+
+time = 1416
+day = "tuesday"
+
+if time < 1200 and time > 500 :
+    print("Good Morning")
+elif time < 1700:
+    print("Good Afternoon")
+    id day != "saturday" and day != "sunday"
+elif time <2000: 
+    print("Good Evening")
+else :
+    print("Good Night")
+
+
+print("code is done")
+
